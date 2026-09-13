d6e3b64f3c8f8aec87b54c559ea7a484286a4b03
Artemiy <bogenkoart@gmail.com>
2026-09-13 15:30:59 +0300

    first commit

---
+++ cmd/textstat/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/bogenkoart/go-training/internal/textstat"
+
+func main() {
+	_ = textstat
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bogenkoart/go-training
+
+go 1.25.3
